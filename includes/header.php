46e2affd5299953b04795caa712bb1e3019d8f88
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-07 11:49:21 +0700

    first commit

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/customer.css">
+    
+    <title>PHP Developer</title>
+
+</head>
+
+<body>
