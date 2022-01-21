304dc716ac86bba691394c22b3fd9cdee73fd221
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-21 11:05:25 +0700

    update

--- admin/code.php
+++ admin/code.php
@@ -1,6 +1,17 @@
+<?php include("includes/dbconnect.php");?>
 <?php
 if(isset($_POST['login_btn'])){
-    echo $email_login = $_POST['email'];
-    echo $password_login = $_POST['password'];
+     $email_login = $_POST['email'];
+     $password_login = $_POST['password'];
+
+    $query="SELECT * FROM users WHERE email='$email_login' AND password='$password_login' ";
+    $query_run=mysqli_query($conn,$query);
+
+    if(mysqli_fetch_array($query_run)){
+        echo"OK";
+    }
+    else{
+        echo"NO";
+    }
 }
 ?>
