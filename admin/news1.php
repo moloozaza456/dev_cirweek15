f5035d39cc2a2d02616377be587db8b25702fe17
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-28 10:30:48 +0700

    add

--- admin/news1.php
+++ admin/news1.php
@@ -13,6 +13,54 @@
         <h4 class="text-success">นายมาร์ก | ผู้พัฒนาเว็บไซต์</h4>
     </div>
     <!-- Content Row -->
+
+
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addAdminNews" data-whatever="@mdo">Add News</button>
+
+    <div class="modal fade" id="addAdminNews" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">ข่าวประชาสัมพันธ์</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <form action="news_code.php" method="post">
+                        <div class="form-group">
+                            <label class="col-form-label">หัวข้อข่าว</label>
+                            <textarea name="head" class="form-control" required placeholder="เพิ่ม-หัวข้อข่าว"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-form-label">รายละเอียดข่าว:</label>
+                            <textarea rows="4" cols="50" name="detail" required class="form-control" placeholder="เพิ่ม-รายละเอียดข่าว"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-form-label">รายละเอียดข่าว: เช่น *.jpg,png</label>
+                            <input type="file" name="hotnew_image" required class="form-control-file" placeholder="เพิ่ม-รูปภาพ">
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-form-label">แนบไฟล์: เช่น *.pdf,doc,xls,ppt,ra</label>
+                            <input type="file" name="hotnew_doc" class="form-control-file" placeholder="แนบไฟล์">
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" name="news_save" class="btn btn-primary">บันทึก</button>
+                        </div>
+                    </form>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
 </div>
 <!-- /.container-fluid -->
 
