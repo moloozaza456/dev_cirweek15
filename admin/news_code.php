2656ca7ac2c1b7502f7d981b9f3f68aeb0ca4819
patipan-patisampita <patipan.patisampita@gmail.com>
2022-02-18 09:34:20 +0700

    update

---
+++ admin/news_code.php
@@ -0,0 +1,36 @@
+<?php include("security.php"); ?>
+<?php include("config/dbconnect.php"); ?>
+<?php include("includes/thaidate.php"); ?>
+
+<?php
+if (isset($_POST['news_save'])) {
+    $head = $_POST['head'];
+    $detail = $_POST['detail'];
+    $namepic = $_FILES["hotnew_image"]['name'];
+    $namedoc = $_FILES['hotnew_doc']['name'];
+    if (file_exists("upload/hotnew_images/" . $_FILES["hotnew_image"]["name"])) {
+        $store = $_FILES["hotnew_image"]["name"];
+        $_SESSION['status'] = "กรุณาเพิ่มรูปภาพ'.$store.' ";
+        echo "กรุณาเพิ่มรูปภาพ";
+        header('Location: news1.php');
+    } else {
+        $design = "ผู้ดูแลระบบ-วท.ตราด";
+        $hotcount = 0;
+        $query = "INSERT INTO hotnew(head,detail,date,namepic,namedoc,hotcount,design) 
+            VALUES('$head','$detail','$date','$namepic','$namedoc','$hotcount','$design')";
+        $result = mysqli_query($con, $query);
+
+        if ($result) {
+            move_uploaded_file($_FILES["hotnew_image"]["tmp_name"], "upload/hotnew_images/" . $_FILES["hotnew_image"]["name"]);
+            move_uploaded_file($_FILES["hotnew_doc"]["tmp_name"], "upload/hotnew_docs/" . $_FILES["hotnew_doc"]["name"]);
+            $_SESSION['success'] = "เพิ่มข่าวสารเรียบร้อยแล้ว";
+            echo "เพิ่มข่าวสารเรียบร้อยแล้ว";
+            header('Location: news1.php');
+        } else {
+            $_SESSION['success'] = "เพิ่มข่าวสารยังไม่เรียบร้อยแล้ว";
+            echo "เพิ่มข่าวสารยังไม่เรียบร้อยแล้ว";
+            header('Location: news1.php');
+        }
+    }
+}
+?>
