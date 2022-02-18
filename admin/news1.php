32a8dc5861ed3cec5646bad325abf9e164711662
patipan-patisampita <patipan.patisampita@gmail.com>
2022-02-18 10:13:10 +0700

    update

--- admin/news1.php
+++ admin/news1.php
@@ -38,56 +38,97 @@
             ?>
 
             <div class="table-responsive">
-                <?php
-                $query = "SELECT * FROM hotnew";
-                $res = mysqli_query($con, $query);
-                ?>
-                <table class="table table-bordered border-primary">
-                    <thead>
-                        <tr>
-                            <th scope="col">ลำดับ</th>
-                            <th scope="col">หัวข้อ</th>
-                            <th scope="col">รายละเอียด</th>
-                            <th scope="col">วันเวลา </th>
-                            <th scope="col">แก้ไข</th>
-                            <th scope="col">ลบข่าว</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $i = 1;
-                        while ($row = mysqli_fetch_array($res)) {
-                        ?>
+            <?php
+                    $perpage = 1;
+                    if (isset($_GET['page']) & !empty($_GET['page'])) {
+                        $curpage = $_GET['page'];
+                    } else {
+                        $curpage = 1;
+                    }
+                    $start = ($curpage * $perpage) - $perpage;
+                    $PageSql = "SELECT * FROM  hotnew ";
+                    $pageres = mysqli_query($con, $PageSql);
+                    $totalres = mysqli_num_rows($pageres);
 
+                    $endpage = ceil($totalres / $perpage);
+                    $startpage = 1;
+                    $nextpage = $curpage + 1;
+                    $previouspage = $curpage - 1;
+
+                    $query = "SELECT * FROM hotnew ORDER by id DESC LIMIT $start, $perpage";
+                    $res = mysqli_query($con, $query);
+                    ?>
+
+<table class="table table-bordered border-primary">
+                        <thead>
                             <tr>
-                                <th scope="row"><?php echo $i; ?></th>
-                                <td><?php echo $row['head']; ?></php>
-                                </td>
-                                <td><?php echo $row['detail']; ?></td>
-                                <td><?php echo $row['date']; ?></td>
-
-                                <td>
-                                    <form action="news_edit.php" method="post">
-                                        <input type="hidden" name="news_edit_id">
-                                        <button type="submit" name="news_editbtn" class="btn btn-success">EDIT</button>
-                                    </form>
-                                </td>
-                                <td>
-                                    <form action="news_code.php" method="post">
-                                        <input type="hidden" name="delete_image">
-                                        <input type="hidden" name="delete_doc">
-                                        <input type="hidden" name="delete_id">
-                                        <button type="submit" name="news_deletebtn" class="btn btn-danger">DELETE</button>
-                                    </form>
-                                </td>
+                                <th scope="col">ลำดับ</th>
+                                <th scope="col">หัวข้อ</th>
+                                <th scope="col">รายละเอียด</th>
+                                <th scope="col">วันเวลา</th>
+                                <th>แก้ไข</th>
+                                <th>ลบข่าว</th>
                             </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            while ($row = mysqli_fetch_array($res)) {
+                            ?>
+                                <tr>
+                                    <th scope="row"><?php echo $i; ?></th>
+                                    <td><?php echo $row['head']; ?></php>
+                                    </td>
+                                    <td><?php echo $row['detail']; ?></td>
+                                    <td><?php echo $row['date']; ?></td>
+                                    <td>
+                                        <form action="news_edit.php" method="post">
+                                            <input type="hidden" name="news_edit_id" value="<?php echo $row['id'] ?>">
+                                            <button type="submit" name="news_editbtn" class="btn btn-success">แก้ไข</button>
+                                        </form>
+                                    </td>
+                                    <td>
+                                        <form action="news_code.php" method="post">
+                                            <input type="hidden" name="delete_image" value="<?php echo $row['namepic'] ?>">
+                                            <input type="hidden" name="delete_doc" value="<?php echo $row['namedoc'] ?>">
+                                            <input type="hidden" name="delete_id" value="<?php echo $row['id'] ?>">
+                                            <button type="submit" name="news_deletebtn" class="btn btn-danger">ลบ</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php
+                                $i++;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
 
-                        <?php
-                            $i++;
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination">
+                            <?php if ($curpage != $startpage) { ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="?page=<?php echo $startpage ?>" tabindex="-1" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                        <span class="sr-only">First</span>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                            <?php if ($curpage >= 2) { ?>
+                                <li class="page-item"><a class="page-link" href="?page=<?php echo $previouspage ?>"><?php echo $previouspage ?></a></li>
+                            <?php } ?>
+                            <li class="page-item active"><a class="page-link" href="?page=<?php echo $curpage ?>"><?php echo $curpage ?></a></li>
+                            <?php if ($curpage != $endpage) { ?>
+                                <li class="page-item"><a class="page-link" href="?page=<?php echo $nextpage ?>"><?php echo $nextpage ?></a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="?page=<?php echo $endpage ?>" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                        <span class="sr-only">Last</span>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </nav>
+
             </div>
         </div>
 
